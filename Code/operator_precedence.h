4a01b9c7598a4f061867a0804390a7688435eb76
tanay-jaiman <109558102+tanay-jaiman@users.noreply.github.com>
2024-07-27 18:24:00 +0530

    Tried completing parse_expression by creating operator precedence table along with functions that help reduce a candidate rule. It still needs completion - probably another day (minus the debug) I believe the only way we'll know if everything works fine is once the execute files are completed as well and from there, I can work on figuring out what and how this thing should really work and possibly a pre-release can be sent out on github.

---
+++ Code/operator_precedence.h
@@ -0,0 +1,60 @@
+#include "instruction.h"
+
+using namespace std;
+
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+#pragma once
+
+enum precedence {LESS_PRECEDENCE, EQUAL_PRECEDENCE, MORE_PRECEDENCE, ACCEPT_PRECEDENCE, ERROR_PRECEDENCE};
+
+enum stack_node_type {TERMINAL, EXPRESSION, ERROR_STACK_NODE};
+
+struct stack_node {
+    stack_node_type type = ERROR_STACK_NODE;
+    Token terminal;
+    Expression * expression = nullptr;
+};
+
+/* Stack Class : Contains stack that stores terminals/expressions for operator precedence operations */
+class Stack {
+    public:
+        Stack() {
+            stack_node dollar;
+            dollar.terminal.token_type = END_OF_FILE;
+            dollar.type = TERMINAL;
+
+            stack.push_back(dollar);
+        }
+
+        stack_node peek_terminal();
+        void push_back(Token);
+        void push_back(Expression*);
+
+        stack_node pop_back();
+        stack_node back();
+        stack_node get(int index);
+
+    private:
+        vector<stack_node> stack;
+};
+
+/* OperatorPrecedence Class : Responsible for creating operator precedence table and getting precedence values. */
+class OperatorPrecedence {
+    private:
+        void create_table();
+        void create_row(string);
+        int get_index(TokenType);
+
+        vector<vector<precedence>> table;
+
+    public:
+        OperatorPrecedence() {
+            create_table();
+        }
+
+        precedence get_precedence(TokenType, TokenType);
+};
+
+#endif
